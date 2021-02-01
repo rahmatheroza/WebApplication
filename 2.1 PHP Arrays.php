8df3a11ace6648f182d36bc89b14dfe314255b67
rahmatheroza <rahmatheroza@gmail.com>
2021-02-01 22:08:07 +0700

    Update 2.1 PHP Arrays.php

--- 2.1 PHP Arrays.php
+++ 2.1 PHP Arrays.php
@@ -1,7 +1,26 @@
+<pre>
 <?php
-    $stuff = array("name" => "Chuck",
-                   "course" => "SI664");
-    foreach($stuff as $k => $v ) {
-       echo "Key=",$k," Val=",$v,"\n";
-    }
+$products = array(
+    'paper' =>	array( 
+        'copier' => "Copier & Multipurpose", 
+        'inkjet' => "Inkjet Printer", 
+        'laser' => "Laser Printer", 
+        'photo' => "Photographic Paper"),
+    'pens' => array(
+        'ball'	=> "Ball Point", 
+        'hilite' => "Highlighters", 
+        'marker' => "Markers"),
+    'misc' => array( 
+        'tape'	=> "Sticky Tape", 
+        'glue'	=> "Adhesives", 
+        'clips' => "Paperclips") 
+);
+foreach ($products as $keyArray => $valueArray) {
+	echo "$keyArray\n";
+	foreach ($valueArray as $key => $value) {
+		echo "$key:$value\n";
+	}
+	echo "\n";
+}
 ?>
+</pre>

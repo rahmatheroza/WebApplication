d95628d4e230d03f29fbc725a278550870bcf260
rahmatheroza <rahmatheroza@gmail.com>
2021-02-09 21:46:52 +0700

    Update 3.3 Modularity.php

--- 3.3 Modularity.php
+++ 3.3 Modularity.php
@@ -1,6 +1,6 @@
 <?php
-include "header.php";
-include "top.php";
+include "header.php"; // file ini terdapat di repo
+include "top.php"; // file ini disengaja tidak ada
 ?>
 <h1>FSI109117 Pemrograman Web 2</h1>
 <p>
@@ -12,5 +12,5 @@ include "top.php";
 	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 </p>
 <?php
-include "footer.php";
+include "footer.php"; // file ini terdapat di repo
 ?>

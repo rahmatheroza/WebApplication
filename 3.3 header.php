6d6c66572c4e5d6d3a125303bbf6fc0792748a3f
rahmatheroza <rahmatheroza@gmail.com>
2021-02-09 21:42:34 +0700

    Create 3.3 header.php

---
+++ 3.3 header.php
@@ -0,0 +1,7 @@
+<?php
+echo "<ul style='padding-inline-start:0px'>
+	<li style='display: inline;'>Home</li>
+	<li style='display: inline;'>News</li>
+	<li style='display: inline;'>Contact</li>
+</ul>";
+?>
